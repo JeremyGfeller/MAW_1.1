18408af7c984c51e17fb5fae3bc5a812bb6b0d49
JeremyGfeller <jeremy.gfeller@cpnv.ch>
2018-10-05 08:32:31 +0200

    Changement de la vue sur l'application
    Utilisation de webBrowser pour avoir un affichage comme l'explorateur windows

--- code_jeremy/maw1.1/maw1.1/Form1.cs
+++ code_jeremy/maw1.1/maw1.1/Form1.cs
@@ -14,126 +14,34 @@ namespace maw1._1
     public partial class Form1 : Form
     {
         List<string> listFiles = new List<string>();
+        FolderBrowserDialog fbd = new FolderBrowserDialog();
         public Form1()
         {
             InitializeComponent();
         }
 
         private void btnOpen_Click(object sender, EventArgs e)
-        {
-            listFiles.Clear();
-            listView.Items.Clear();
-
-            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
-                
+        {                
             if (fbd.ShowDialog() == DialogResult.OK)
                 txtPath.Text = fbd.SelectedPath;
-
-            /*using (FolderBrowserDialog fbd = new FolderBrowserDialog())
-            {
-                if(fbd.ShowDialog() == DialogResult.OK)
-                {
-                    string[] files = Directory.GetFiles(fbd.SelectedPath);
-                    string[] directorys = Directory.GetDirectories(fbd.SelectedPath);
-
-                    foreach (string file in files)
-                    {
-                        imageList.Images.Add(System.Drawing.Icon.ExtractAssociatedIcon(file));
-                        FileInfo fi = new FileInfo(file);
-                        listFiles.Add(fi.FullName);
-                        listView.Items.Add(fi.Name, imageList.Images.Count - 1);
-                        //https://www.youtube.com/watch?v=EQa_3YiUicw
-                    }
-
-                    foreach (string directory in directorys)
-                    {
-                        FileInfo fi = new FileInfo(directory);
-                        listFiles.Add(fi.FullName);
-                        listView.Items.Add(fi.Name, Path.GetFileName(directory));
-                    }
-
-                    txtPath.Text = fbd.SelectedPath;
-
-                    //https://www.c-sharpcorner.com/article/display-sub-directories-and-files-in-treeview/
-
-                    // A VOIR !!
-                    // https://www.youtube.com/watch?v=aonRoEokQeY
-                }
-            }*/
+            
+            // A VOIR !!
+            // https://www.youtube.com/watch?v=aonRoEokQeY
         }
 
         private void btnSearch_Click(object sender, EventArgs e)
         {
-            treeView1.Nodes.Clear();
-
-            toolTip1.ShowAlways = true;
-            if (txtPath.Text != "" && Directory.Exists(txtPath.Text))
-                LoadDirectory(txtPath.Text);
-            else
-                MessageBox.Show("Select Directory!!");
+            webBrowser.Url = new Uri(fbd.SelectedPath);
         }
 
-        public void LoadDirectory(string Dir)
+        private void btnGoBack_Click(object sender, EventArgs e)
         {
-            DirectoryInfo di = new DirectoryInfo(Dir);
-            TreeNode tds = treeView1.Nodes.Add(di.Name);
-            tds.Tag = di.FullName;
-            tds.StateImageIndex = 0;
-            LoadFiles(Dir, tds);
-            LoadSubDirectories(Dir, tds);
+            webBrowser.GoBack();
         }
 
-        private void LoadFiles(string dir, TreeNode td)
+        private void btnGoForward_Click(object sender, EventArgs e)
         {
-            string[] Files = Directory.GetFiles(dir, "*.*");
-
-            // Loop through them to see files  
-            foreach (string file in Files)
-            {
-                FileInfo fi = new FileInfo(file);
-                TreeNode tds = td.Nodes.Add(fi.Name);
-                tds.Tag = fi.FullName;
-                tds.StateImageIndex = 1;
-            }
-        }
-
-        private void LoadSubDirectories(string dir, TreeNode td)
-        {
-            // Get all subdirectories  
-            string[] subdirectoryEntries = Directory.GetDirectories(dir);
-            // Loop through them to see if they have any other subdirectories  
-            foreach (string subdirectory in subdirectoryEntries)
-            {
-                DirectoryInfo di = new DirectoryInfo(subdirectory);
-                TreeNode tds = td.Nodes.Add(di.Name);
-                tds.StateImageIndex = 0;
-                tds.Tag = di.FullName;
-                LoadFiles(subdirectory, tds);
-                LoadSubDirectories(subdirectory, tds);
-            }
-        }
-
-        private void treeView1_MouseMove(object sender, MouseEventArgs e)
-        {
-            // Get the node at the current mouse pointer location.  
-            TreeNode theNode = this.treeView1.GetNodeAt(e.X, e.Y);
-
-            // Set a ToolTip only if the mouse pointer is actually paused on a node.  
-            if (theNode != null && theNode.Tag != null)
-            {
-                // Change the ToolTip only if the pointer moved to a new node.  
-                if (theNode.Tag.ToString() != this.toolTip1.GetToolTip(this.treeView1))
-                    this.toolTip1.SetToolTip(this.treeView1, theNode.Tag.ToString());
-
-            }
-            else     // Pointer is not over a node so clear the ToolTip.  
-            {
-                this.toolTip1.SetToolTip(this.treeView1, "");
-            }
-        }
-        private void btnOpenFile_Click(object sender, EventArgs e)
-        {
-
+            webBrowser.GoForward();
         }
     }
 }
