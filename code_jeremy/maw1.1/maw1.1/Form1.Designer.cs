c4c3a4063cc8487d10ee8187142aa81d74cd1547
JeremyGfeller <jeremy.gfeller@cpnv.ch>
2018-11-07 09:23:41 +0100

    Enelver des commentaires, ajout d'un bouton pour effacer les tris entrés

--- code_jeremy/maw1.1/maw1.1/Form1.Designer.cs
+++ code_jeremy/maw1.1/maw1.1/Form1.Designer.cs
@@ -47,6 +47,7 @@ namespace maw1._1
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.txtSort4 = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnOpen
@@ -162,11 +163,22 @@ namespace maw1._1
             this.txtSort4.Size = new System.Drawing.Size(100, 20);
             this.txtSort4.TabIndex = 13;
             // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(615, 118);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 15;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(796, 538);
+            this.Controls.Add(this.btnClear);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.txtSort4);
             this.Controls.Add(this.label3);
@@ -206,6 +218,7 @@ namespace maw1._1
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txtSort4;
+        private System.Windows.Forms.Button btnClear;
     }
 }
 
--- code_jeremy/maw1.1/maw1.1/Form1.cs
+++ code_jeremy/maw1.1/maw1.1/Form1.cs
@@ -27,38 +27,6 @@ namespace maw1._1
                 
             if (fbd.ShowDialog() == DialogResult.OK)
                 txtPath.Text = fbd.SelectedPath;
-
-            /*using (FolderBrowserDialog fbd = new FolderBrowserDialog())
-            {
-                if(fbd.ShowDialog() == DialogResult.OK)
-                {
-                    string[] files = Directory.GetFiles(fbd.SelectedPath);
-                    string[] directorys = Directory.GetDirectories(fbd.SelectedPath);
-
-                    foreach (string file in files)
-                    {
-                        imageList.Images.Add(System.Drawing.Icon.ExtractAssociatedIcon(file));
-                        FileInfo fi = new FileInfo(file);
-                        listFiles.Add(fi.FullName);
-                        listView.Items.Add(fi.Name, imageList.Images.Count - 1);
-                        //https://www.youtube.com/watch?v=EQa_3YiUicw
-                    }
-
-                    foreach (string directory in directorys)
-                    {
-                        FileInfo fi = new FileInfo(directory);
-                        listFiles.Add(fi.FullName);
-                        listView.Items.Add(fi.Name, Path.GetFileName(directory));
-                    }
-
-                    txtPath.Text = fbd.SelectedPath;
-
-                    //https://www.c-sharpcorner.com/article/display-sub-directories-and-files-in-treeview/
-
-                    // A VOIR !!
-                    // https://www.youtube.com/watch?v=aonRoEokQeY
-                }
-            }*/
         }
 
         private void btnSearch_Click(object sender, EventArgs e)
@@ -288,6 +256,14 @@ namespace maw1._1
             {
                 this.toolTip1.SetToolTip(this.treeView1, "");
             }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtSort.Clear();
+            txtSort2.Clear();
+            txtSort3.Clear();
+            txtSort4.Clear();
         }
     }
 }
