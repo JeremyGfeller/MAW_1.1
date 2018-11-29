9d67862744c3b888765f12917e3b2760f3b24125
JUNOD Alexandre <Alexandre.JUNOD@cpnv.ch>
2018-11-29 13:35:58 +0100

    Tentative lecture excel

--- Finder/Finder/Class/ReadWord.cs
+++ Finder/Finder/Class/ReadWord.cs
@@ -15,6 +15,7 @@ using System.Security.Principal;
 using System.Windows.Input;
 using iTextSharp.text.pdf;
 using iTextSharp.text.pdf.parser;
+using Spire.Xls.Core.Spreadsheet.Shapes;
 
 namespace Finder.Class
 {
@@ -24,6 +25,65 @@ namespace Finder.Class
         {
             //N:\COMMUN\ELEVE\INFO\SI-T1a\MawDataTest
 
+
+            bool SortResult = CompletePath.ToLower().Contains(".xls");
+            StringBuilder text = new StringBuilder();
+            if (SortResult)
+            {
+                Workbook workbook = new Workbook();
+                workbook.LoadFromFile(CompletePath);
+                Worksheet sheet = workbook.Worksheets[0];
+                int columnCount = sheet.Columns.Count();
+                int rowsCount = sheet.Rows.Count();
+                int i = 0;
+                int j = 0;
+                foreach (CellRange rows in sheet.Rows[0])
+                {
+                    try
+                    {
+                        foreach (CellRange range in sheet.Columns[0])
+                        {
+                            text.AppendLine(range.Text + rows.Text);
+
+                            if (j > rowsCount)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                j++;
+                            }
+                        }
+                        if (i > rowsCount)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            i++;
+                        }
+                    }
+                    catch
+                    {
+
+                    }
+                    
+                }
+            }
+            bool ContentCorrespond; //Used to know if the content correspond with the values given by the user
+            String txt_keyWord = FileFinder.txt_keyWord.Text; //Store the value given by the user
+            ContentCorrespond = text.ToString().ToLower().Contains(txt_keyWord.ToLower()); //Return 1 if the string countains the sort typed by the user. ToLower() to make the sort case insensitive
+            return ContentCorrespond; 
+
+
+
+
+
+
+
+
+
+            /* Fonctionnel word
             bool SortResult = CompletePath.ToLower().Contains(".txt");
             bool SortResult2 = CompletePath.ToLower().Contains(".doc");
             StringBuilder text = new StringBuilder();
@@ -44,7 +104,7 @@ namespace Finder.Class
             bool ContentCorrespond; //Used to know if the content correspond with the values given by the user
             String txt_keyWord = FileFinder.txt_keyWord.Text; //Store the value given by the user
             ContentCorrespond = text.ToString().ToLower().Contains(txt_keyWord.ToLower()); //Return 1 if the string countains the sort typed by the user. ToLower() to make the sort case insensitive
-            return ContentCorrespond;
+            return ContentCorrespond;*/
 
 
             //Fonctionnel lecture PDF
