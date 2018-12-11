e17656d7019e481d064c9618ec520f3e7ab0221b
JUNOD Alexandre <Alexandre.JUNOD@cpnv.ch>
2018-12-11 14:27:46 +0100

    Update UX

--- Finder/Finder/Finder.Designer.cs
+++ Finder/Finder/Finder.Designer.cs
@@ -49,12 +49,13 @@
             this.btn_openFichier = new System.Windows.Forms.Button();
             this.btn_search = new System.Windows.Forms.Button();
             this.btn_cancel = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn_path
             // 
             this.btn_path.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_path.Location = new System.Drawing.Point(202, 79);
+            this.btn_path.Location = new System.Drawing.Point(286, 76);
             this.btn_path.Name = "btn_path";
             this.btn_path.Size = new System.Drawing.Size(106, 27);
             this.btn_path.TabIndex = 11;
@@ -64,17 +65,17 @@
             // 
             // txt_path
             // 
-            this.txt_path.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_path.Location = new System.Drawing.Point(87, 119);
+            this.txt_path.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_path.Location = new System.Drawing.Point(53, 77);
             this.txt_path.Name = "txt_path";
-            this.txt_path.Size = new System.Drawing.Size(339, 22);
+            this.txt_path.Size = new System.Drawing.Size(210, 26);
             this.txt_path.TabIndex = 12;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(96, 25);
+            this.label1.Location = new System.Drawing.Point(47, 22);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(320, 33);
             this.label1.TabIndex = 13;
@@ -84,7 +85,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(737, 25);
+            this.label2.Location = new System.Drawing.Point(502, 22);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(344, 33);
             this.label2.TabIndex = 14;
@@ -93,7 +94,7 @@
             // lbl_keyWord
             // 
             this.lbl_keyWord.AutoSize = true;
-            this.lbl_keyWord.Location = new System.Drawing.Point(720, 79);
+            this.lbl_keyWord.Location = new System.Drawing.Point(507, 75);
             this.lbl_keyWord.Name = "lbl_keyWord";
             this.lbl_keyWord.Size = new System.Drawing.Size(52, 13);
             this.lbl_keyWord.TabIndex = 15;
@@ -102,7 +103,7 @@
             // lbl_author
             // 
             this.lbl_author.AutoSize = true;
-            this.lbl_author.Location = new System.Drawing.Point(720, 119);
+            this.lbl_author.Location = new System.Drawing.Point(812, 75);
             this.lbl_author.Name = "lbl_author";
             this.lbl_author.Size = new System.Drawing.Size(38, 13);
             this.lbl_author.TabIndex = 16;
@@ -111,7 +112,7 @@
             // lbl_date
             // 
             this.lbl_date.AutoSize = true;
-            this.lbl_date.Location = new System.Drawing.Point(720, 158);
+            this.lbl_date.Location = new System.Drawing.Point(507, 134);
             this.lbl_date.Name = "lbl_date";
             this.lbl_date.Size = new System.Drawing.Size(104, 13);
             this.lbl_date.TabIndex = 17;
@@ -120,7 +121,7 @@
             // lbl_files
             // 
             this.lbl_files.AutoSize = true;
-            this.lbl_files.Location = new System.Drawing.Point(720, 200);
+            this.lbl_files.Location = new System.Drawing.Point(812, 135);
             this.lbl_files.Name = "lbl_files";
             this.lbl_files.Size = new System.Drawing.Size(75, 13);
             this.lbl_files.TabIndex = 18;
@@ -128,34 +129,34 @@
             // 
             // txt_keyWord
             // 
-            this.txt_keyWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_keyWord.Location = new System.Drawing.Point(914, 74);
+            this.txt_keyWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_keyWord.Location = new System.Drawing.Point(510, 91);
             this.txt_keyWord.Name = "txt_keyWord";
-            this.txt_keyWord.Size = new System.Drawing.Size(167, 22);
+            this.txt_keyWord.Size = new System.Drawing.Size(259, 26);
             this.txt_keyWord.TabIndex = 19;
             // 
             // txt_author
             // 
-            this.txt_author.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_author.Location = new System.Drawing.Point(914, 114);
+            this.txt_author.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_author.Location = new System.Drawing.Point(815, 92);
             this.txt_author.Name = "txt_author";
-            this.txt_author.Size = new System.Drawing.Size(167, 22);
+            this.txt_author.Size = new System.Drawing.Size(259, 26);
             this.txt_author.TabIndex = 20;
             // 
             // txt_date
             // 
-            this.txt_date.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_date.Location = new System.Drawing.Point(914, 158);
+            this.txt_date.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_date.Location = new System.Drawing.Point(510, 150);
             this.txt_date.Name = "txt_date";
-            this.txt_date.Size = new System.Drawing.Size(167, 22);
+            this.txt_date.Size = new System.Drawing.Size(259, 26);
             this.txt_date.TabIndex = 21;
             // 
             // txt_file
             // 
-            this.txt_file.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_file.Location = new System.Drawing.Point(914, 200);
+            this.txt_file.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_file.Location = new System.Drawing.Point(815, 151);
             this.txt_file.Name = "txt_file";
-            this.txt_file.Size = new System.Drawing.Size(167, 22);
+            this.txt_file.Size = new System.Drawing.Size(259, 26);
             this.txt_file.TabIndex = 22;
             // 
             // lst_files
@@ -165,9 +166,9 @@
             this.Taille,
             this.Auteur,
             this.Date});
-            this.lst_files.Location = new System.Drawing.Point(87, 311);
+            this.lst_files.Location = new System.Drawing.Point(53, 295);
             this.lst_files.Name = "lst_files";
-            this.lst_files.Size = new System.Drawing.Size(994, 266);
+            this.lst_files.Size = new System.Drawing.Size(1021, 266);
             this.lst_files.TabIndex = 23;
             this.lst_files.UseCompatibleStateImageBehavior = false;
             this.lst_files.View = System.Windows.Forms.View.Details;
@@ -194,9 +195,9 @@
             // 
             // btn_openDirectory
             // 
-            this.btn_openDirectory.Location = new System.Drawing.Point(392, 592);
+            this.btn_openDirectory.Location = new System.Drawing.Point(765, 574);
             this.btn_openDirectory.Name = "btn_openDirectory";
-            this.btn_openDirectory.Size = new System.Drawing.Size(146, 23);
+            this.btn_openDirectory.Size = new System.Drawing.Size(146, 28);
             this.btn_openDirectory.TabIndex = 24;
             this.btn_openDirectory.Text = "Ouvrir le répertoire ";
             this.btn_openDirectory.UseVisualStyleBackColor = true;
@@ -204,9 +205,9 @@
             // 
             // btn_openFichier
             // 
-            this.btn_openFichier.Location = new System.Drawing.Point(626, 592);
+            this.btn_openFichier.Location = new System.Drawing.Point(928, 574);
             this.btn_openFichier.Name = "btn_openFichier";
-            this.btn_openFichier.Size = new System.Drawing.Size(146, 23);
+            this.btn_openFichier.Size = new System.Drawing.Size(146, 28);
             this.btn_openFichier.TabIndex = 25;
             this.btn_openFichier.Text = "Ouvrir le fichier";
             this.btn_openFichier.UseVisualStyleBackColor = true;
@@ -215,9 +216,9 @@
             // btn_search
             // 
             this.btn_search.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
-            this.btn_search.Location = new System.Drawing.Point(987, 244);
+            this.btn_search.Location = new System.Drawing.Point(510, 196);
             this.btn_search.Name = "btn_search";
-            this.btn_search.Size = new System.Drawing.Size(94, 23);
+            this.btn_search.Size = new System.Drawing.Size(259, 28);
             this.btn_search.TabIndex = 24;
             this.btn_search.Text = "Rechercher";
             this.btn_search.UseVisualStyleBackColor = true;
@@ -226,20 +227,31 @@
             // btn_cancel
             // 
             this.btn_cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
-            this.btn_cancel.Location = new System.Drawing.Point(796, 244);
+            this.btn_cancel.Location = new System.Drawing.Point(815, 196);
             this.btn_cancel.Name = "btn_cancel";
-            this.btn_cancel.Size = new System.Drawing.Size(94, 23);
+            this.btn_cancel.Size = new System.Drawing.Size(259, 28);
             this.btn_cancel.TabIndex = 26;
             this.btn_cancel.Text = "Réinitialiser";
             this.btn_cancel.UseVisualStyleBackColor = true;
             this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(47, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(333, 33);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "Contenu de la recherche";
             // 
             // Finder
             // 
             this.AccessibleName = "Finder";
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1258, 636);
+            this.ClientSize = new System.Drawing.Size(1137, 614);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.btn_cancel);
             this.Controls.Add(this.btn_openFichier);
             this.Controls.Add(this.btn_openDirectory);
@@ -286,5 +298,6 @@
         public System.Windows.Forms.TextBox txt_file;
         public System.Windows.Forms.Button btn_search;
         public System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.Label label3;
     }
 }
